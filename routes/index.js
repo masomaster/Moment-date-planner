0eb183c2e7e97700ed7a33bc430fe21900662ec2
masomaster <masonlancaster@protonmail.com>
2022-10-07 15:40:59 -0700

    implement OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+const isLoggedIn = require('../config/auth');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'San Diego Date Night Planner' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // prompt: "select_account"
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout(function(err) {
+    if (err) {return next (err);}
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
