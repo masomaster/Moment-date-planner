240e9ce83105ae7bab1d52e5055db84ba08a7c43
masomaster <masonlancaster@protonmail.com>
2022-10-15 22:06:52 -0700

    changes to footer; addition of styled index2 mockup page

--- routes/activities.js
+++ routes/activities.js
@@ -4,6 +4,11 @@ const passport = require('passport');
 const isLoggedIn = require('../config/auth');
 const activitiesCtrl = require('../controllers/activities');
 
+// This is for testing. Delete when done
+router.get('/index2', function(req, res) {
+    res.render('activities/index2');
+})
+
 // All routes start with '/activities'
 router.get('/', activitiesCtrl.index);
 router.get('/new', isLoggedIn, activitiesCtrl.new);
@@ -12,5 +17,6 @@ router.get('/:id', activitiesCtrl.show);
 router.get('/:id/edit', isLoggedIn, activitiesCtrl.edit);
 router.put('/:id', isLoggedIn, activitiesCtrl.update);
 router.delete('/:id', isLoggedIn, activitiesCtrl.delete);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/dateplans', datePlansRouter);
 app.use('/activities', activitiesRouter);
+// app.use(express.static('public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
